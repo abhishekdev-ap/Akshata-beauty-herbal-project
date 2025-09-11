f03c4f2c64e310e506a0a2708def40d3c1f3dfe2
apapapapaap <108124553+apapapapaap@users.noreply.github.com>
2025-09-11 20:51:57 +0530

    setting for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/-Parlor-project-Full-stack-development-/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
